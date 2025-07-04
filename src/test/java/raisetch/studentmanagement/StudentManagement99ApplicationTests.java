5b2b5739e7f71b4728fa4ede900ce4dd0201398e
Epi.T <eccossk1@gmail.com>
2025-07-04 10:50:39 +0900

    first commit

---
+++ src/test/java/raisetch/studentmanagement/StudentManagement99ApplicationTests.java
@@ -0,0 +1,13 @@
+package raisetch.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagement99ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
